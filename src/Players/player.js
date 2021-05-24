d72e158aba489d7144c6fc3325e34d0f0b460eb4
youngtrashbag <yuule.smeker@gmail.com>
2021-05-24 15:19:29 +0200

    feat: display portfolio for user

--- src/Players/player.js
+++ src/Players/player.js
@@ -5,6 +5,7 @@ import { PlayerColors } from "../global";
 const PlayerView = () => {
     const { playerId } = useParams();
     const players = JSON.parse(window.sessionStorage.getItem("players"));
+    const currencies = JSON.parse(window.sessionStorage.getItem("currencies"));
 
     if (playerId >= players.length) {
         return (
@@ -12,10 +13,39 @@ const PlayerView = () => {
         );
     }
 
+    var currencyRows = [];
+    var totalFiatValue = 0;
+    for (var i=0; i<currencies.length; i++) {
+        const currencyName = currencies[i].name;
+        const amountOwned = players[playerId].portfolio[i];
+        const fiatValue = players[playerId].portfolio[i] * currencies[i].value;
+
+        totalFiatValue += fiatValue;
+
+        currencyRows.push(
+            <tr>
+                <td>{currencyName}</td>
+                <td>{amountOwned}</td>
+                <td>{fiatValue}</td>
+            </tr>
+        )
+    }
+
     return (
+        <>
         <h1 style={{ color: PlayerColors[playerId] }}>
             Portfolio { players[playerId].name }
         </h1>
+        <h3>Total Guthaben: ${totalFiatValue}</h3>
+        <table>
+            <tr>
+                <th>Currency</th>
+                <th># Owned</th>
+                <th>Fiat Value</th>
+            </tr>
+            {currencyRows}
+        </table>
+        </>
     )
 }
 
