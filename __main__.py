1b1297302184b0cfdfd625e5bb60573b81be7165
youngtrashbag <yuule.smeker@gmail.com>
2021-11-15 20:30:48 +0100

    fix: pythonpath problem

---
+++ __main__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import sys
+from pathlib import Path
+from flask import Flask
+
+from monopoly_invest.api import API
+from monopoly_invest.web import web_frontend
+
+app = Flask(__name__)
+app.register_blueprint(API, url_prefix='/api')
+app.register_blueprint(web_frontend, url_prefix='/')
+
+# TODO: why not available at /static/index.css
+print(web_frontend.static_url_path, web_frontend.static_folder)
+
+app.run(debug=True)
